cfc251e361197bf028cf9c6e95fc3c7cde9bc807
Bhawana0218 <bhawanabishtbhanu1205@gmail.com>
2026-06-06 05:44:09 +0530

    Finalize: Aradhana Astroagent

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .api.routes import router
+from api.routes import router
 
 dotenv_path = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path)
